fe0d63574e252d6b5a65ed36c95f9818dc2fd8e5
Christophe Boutter <boutta@gmail.com>
2012-06-29 14:31:45 +0200

    Renew annotations ins models.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,18 @@
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :integer         not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
 class Micropost < ActiveRecord::Base
   attr_accessible :content
   belongs_to :user
 
   validates :user_id, presence: true
 end
+
+
