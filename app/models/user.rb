61701d8c39b09bf792a6c4e9edac9ca70fc982aa
Christophe Boutter <boutta@gmail.com>
2012-06-29 14:17:45 +0200

    Associate User and Microposts.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
+  has_many :microposts
   
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
